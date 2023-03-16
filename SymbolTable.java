10dc78536af8551eb34d0532690450194e86d7c1
Zeynep Erdogru <zerdogru@fred.socs.uoguelph.ca>
2023-03-16 17:30:47 -0400

    added a hashmap to SymbolTable

--- SymbolTable.java
+++ SymbolTable.java
@@ -6,8 +6,12 @@ import absyn.*;
  * SymbolTable
  */
 public class SymbolTable {
+    private Hashmap<String, ArrayList<NodeType>>table;
+
+
     public SymbolTable() {
         System.out.println("Created a SemanticAnalyzer object. ");
     }
+]
     
 }
